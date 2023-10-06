3650a3b270fc73e40d25c277a9f0d87a16518bdd
Ningyuan Li <mariotaku.lee@gmail.com>
2023-10-06 22:51:26 +0900

    check for rtk hardware for LGNC audio

--- modules/webos/lgnc/lgnc_module.c
+++ modules/webos/lgnc/lgnc_module.c
@@ -4,6 +4,7 @@
 #include <lgnc_plugin.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <dlfcn.h>
 
 pthread_mutex_t SS4S_LGNC_Lock = PTHREAD_MUTEX_INITIALIZER;
 bool SS4S_LGNC_Initialized = false;
@@ -18,6 +19,29 @@ SS4S_EXPORTED bool SS4S_ModuleOpen_LGNC(SS4S_Module *module, const SS4S_LibraryC
     module->AudioDriver = &SS4S_LGNC_AudioDriver;
     module->VideoDriver = &SS4S_LGNC_VideoDriver;
     return true;
+}
+
+SS4S_EXPORTED SS4S_ModuleCheckFlag SS4S_ModuleCheck_LGNC(SS4S_ModuleCheckFlag flags) {
+    if (flags & SS4S_MODULE_CHECK_AUDIO) {
+        /*
+         * A simple check for unsupported hardware. A partial solution is to use ALSA/PulseAudio, so we fail the check
+         * if we have the problematic library, and let SS4S choose other modules
+         * Related issues:
+         * https://github.com/mariotaku/moonlight-tv/issues/110
+         * https://github.com/mariotaku/moonlight-tv/issues/184
+         * https://github.com/mariotaku/ihsplay/issues/13
+         */
+        void *lib = dlopen("libkadaptor.so.1", RTLD_LAZY);
+        if (lib != NULL) {
+            bool has_flow = dlsym(lib, "_Z8new_flowv") != NULL;
+            dlclose(lib);
+            if (has_flow) {
+                // If we have new_flow function, report audio model is not compatible
+                return flags & ~SS4S_MODULE_CHECK_AUDIO;
+            }
+        }
+    }
+    return flags;
 }
 
 int SS4S_LGNC_Driver_Init() {
